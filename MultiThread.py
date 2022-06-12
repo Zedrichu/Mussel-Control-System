a2cba0a9ba1fc195796cd2095be3cbe25025bf27
Adrian Zvizdenco <adrzvizdencojr@gmail.com>
2022-06-12 18:13:32 +0200

    Implemented basic multithread scheduler

---
+++ MultiThread.py
@@ -0,0 +1,57 @@
+import time
+
+class TaskScheduler:
+    def __init__(self, ticks):
+        self.ticks = ticks
+        self.taskDict = {}
+        self.clockTime = 0
+    
+    def addTask(self, task):
+        self.taskDict[task.freq] = task
+
+    def run(self):
+        if self.clockTime > self.ticks:
+            self.clockTime = 1
+        else:
+            self.clockTime += 1
+
+        for i in self.taskDict.keys():
+            if self.clockTime % i == 0:
+                self.taskDict[i].run()
+        print("Time"+str(self.clockTime))
+        time.sleep(1)
+
+    def reset(self):
+        self.currentIndex = 0
+        self.clockTime = 0
+    
+class Task:
+    def __init__(self, name, freq, func):
+        self.name = name
+        self.freq = freq
+        self.func = func
+
+    def run(self):  
+        self.func()
+        print(self.name + "executed!")
+        
+def add23():
+    print("2+3")
+
+def add50():
+    print("5+0")
+
+def start():
+    print("Start!")
+
+t1 = Task("t1", 2, start)
+t2 = Task("t2", 3, add23)
+t3 = Task("t3", 5, add50)
+
+schedule = TaskScheduler(10)
+schedule.addTask(t1)
+schedule.addTask(t2)
+schedule.addTask(t3)
+
+while True:
+    schedule.run()
