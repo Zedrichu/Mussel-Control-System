fd59ce2c4f12e177563d9ad19fc53645ef4c0a9c
Adrian Zvizdenco <adrzvizdencojr@gmail.com>
2022-06-14 08:10:26 +0200

    Analyzed new data of experiment overnight

--- utilities/GrapherPIDLogs.py
+++ utilities/GrapherPIDLogs.py
@@ -13,18 +13,20 @@ Description: Script graphing the calibration of PID controller parameters.
 
 import matplotlib.pyplot as plt
 
-file = open("data/dataP8.5I2D0.5-1.txt")
+file = open("data/dataOvernightP8.5I2D0.2.txt")
 dataset = file.readlines()
 timeData = []
 tempData = []
 
-for line in dataset:
+for line in dataset[1:]:
     t,c = map(float, line.split(','))
     tempData.append(c)
     timeData.append(t)
 
 ref = 18
 plt.figure()
+plt.title("PID Controller P-->8.5 I-->2 D-->0.2")
 plt.scatter(timeData, tempData, color = 'k', alpha=0.5)
 plt.plot([0,timeData[-1]], [ref, ref], '--', color = 'r')
+plt.ylim([17,20])
 plt.savefig('PIDplot.png')
